560c7b00d659fa9de60a2a61080c99ed33c2fa8e
AlexMarsh2341 <alexander.charles@icloud.com>
2024-12-17 22:08:45 -0500

    More work on website, started file editing

--- file_uploading/main.py
+++ file_uploading/main.py
@@ -1,6 +1,5 @@
-from distutils.log import debug
-from fileinput import filename
 from flask import *
+from mutagen.easyid3 import EasyID3
 
 app = Flask(__name__)
 
@@ -10,13 +9,26 @@ def main():
     return render_template("index.html")
 
 # runs when file is successfully uploaded
-@app.route('/success', methods=['POST'])
-def success():
+@app.route('/edit', methods=['POST'])
+def edit():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-        return render_template("acknowledgement.html", name=f.filename)
+        return render_template("acknowledgement.html", file = f, name = f.filename)
 
+# runs once new file metadata is uploaded
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        audio = EasyID3(f)
+
+        #edit tags
+        audio["artist"] = 'artist'
+        audio["album"] = 'album'
+        audio["title"] = 'title'
+
+        audio.save()
+        return render_template("final.html", file=audio)
 
 if __name__ == '__main__':
     app.run(debug=True)
