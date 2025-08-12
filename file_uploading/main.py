c367f0eb2ce28200addc2788a48c6d22320a77b7
AlexMarsh2341 <alexander.charles@icloud.com>
2025-08-12 17:26:28 -0400

    More work on website, started file editing

--- file_uploading/main.py
+++ file_uploading/main.py
@@ -1,5 +1,6 @@
 from flask import *
 from mutagen.easyid3 import EasyID3
+import tempfile
 
 app = Flask(__name__)
 
@@ -20,12 +21,20 @@ def edit():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
+        f = request.files.get('file')
+
+        art = request.form['artist']
+        al = request.form['album']
+        ti = request.form['title']
         audio = EasyID3(f)
 
         #edit tags
-        audio["artist"] = 'artist'
-        audio["album"] = 'album'
-        audio["title"] = 'title'
+        audio["artist"] = art
+        print(audio["artist"])
+        audio["album"] = al
+        print(audio["album"])
+        audio["title"] = ti
+        print(audio["title"])
 
         audio.save()
         return render_template("final.html", file=audio)
